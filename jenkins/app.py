423200a072050c8121e9a1b719eaf50fa2c56a2a
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-24 22:00:43 +0700

    Add stages deploy

--- jenkins/app.py
+++ jenkins/app.py
@@ -1,7 +1,12 @@
-def main():
-    print("Selamat datang di Aplikasi Python Sederhana!")
-    print("Ini hanya contoh pesan sederhana.")
-    print("Terima kasih sudah menjalankannya.")
+import subprocess
+import os
 
-if __name__ == "__main__":
-    main()
+# Build the Python application (replace with actual build commands)
+subprocess.run(["python", "-m", "compileall", "sources/"])
+os.makedirs("build", exist_ok=True)
+
+# Run the Python application in development mode
+subprocess.Popen(["python", "app.py"])
+
+print("Now...")
+print("Visit http://localhost:3000 to see your Python application in action.")
