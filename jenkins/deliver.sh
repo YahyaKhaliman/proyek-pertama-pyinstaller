993e836a4645a097857a9006e890a249beee2774
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-24 23:04:10 +0700

    Add stages deploy

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Ganti '/usr/bin/python3' dengan path ke interpreter Python Anda
+PYTHON_EXECUTABLE="/usr/bin/python3"
+
 echo 'The following command runs your Python application:'
 
 set -x
 
-python sources/app.py &
+# Menggunakan path Python yang telah ditentukan
+$PYTHON_EXECUTABLE sources/app.py &
 
 sleep 1
 echo $! > .pidfile
