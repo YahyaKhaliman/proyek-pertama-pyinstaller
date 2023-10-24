1a1c6f03b124dfbcf3324653695bcb88c28c9898
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-24 22:03:01 +0700

    Add stages deploy

--- jenkins/app.py
+++ jenkins/app.py
@@ -1,11 +1,9 @@
 import subprocess
 import os
 
-# Build the Python application (replace with actual build commands)
 subprocess.run(["python", "-m", "compileall", "sources/"])
 os.makedirs("build", exist_ok=True)
 
-# Run the Python application in development mode
 subprocess.Popen(["python", "app.py"])
 
 print("Now...")
