72b35ec16b3dc0b22355082078a934468706e43f
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-24 22:16:18 +0700

    Add stages deploy

--- jenkins/app.py
+++ jenkins/app.py
@@ -1,23 +1,20 @@
 import subprocess
 import time
 
-# Cari tahu lokasi proyek
-project_path = "/var/jenkins_home/workspace/simple-node-js-react-app"
+# Build your Node.js/React application for production
+print("Building your Node.js/React application for production...")
+subprocess.call(["npm", "run", "build"])
 
-# Build the Node.js/React application
-print("Building the Node.js/React application...")
-subprocess.call(["ls", project_path])  # Cek direktori proyek
-subprocess.call(["npm", "run", "build"], cwd=project_path, shell=True)
-
-# Start the Node.js/React application in development mode
-print("Starting the Node.js/React application in development mode...")
-start_process = subprocess.Popen("npm start &", cwd=project_path, shell=True, preexec_fn=os.setsid)
+# Run your Node.js/React application in development mode
+print("Running your Node.js/React application in development mode...")
+start_process = subprocess.Popen("npm start &", shell=True, preexec_fn=os.setsid)
 
 # Wait for a moment to ensure the application is up and running
 time.sleep(1)
 
 # Write the process ID (PID) to a file
-with open(project_path + "/.pidfile", "w") as pidfile:
+with open(".pidfile", "w") as pidfile:
     pidfile.write(str(start_process.pid))
 
-print("The Node.js/React application is running. You can visit http://localhost:3000 to see it in action.")
+print("Now...")
+print("Visit http://localhost:3000 to see your Node.js/React application in action.")
