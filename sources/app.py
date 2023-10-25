171875e97d1fdfbce0795e0302ca27635c2533a5
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-25 11:53:19 +0700

    Add stages deploy

--- sources/app.py
+++ sources/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return "Submission CI/CD Dicoding 2023 Yahya Khaliman Indrayana"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
