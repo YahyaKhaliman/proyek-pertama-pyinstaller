1e10e64e5ff4abce3dead70b67f9c54ebd4bebce
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-25 12:19:04 +0700

    Add stages deploy

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -2,6 +2,13 @@
 
 echo 'Menjalankan aplikasi Python di port 5000...'
 
+# Instal dependensi Streamlit
+pip install streamlit
+
+# Jalankan aplikasi Streamlit
+streamlit run nama_app_streamlit.py
+
+
 # Menjalankan aplikasi Python di latar belakang
 python sources/app.py &
 
