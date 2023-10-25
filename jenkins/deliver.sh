ab2d3dfbc09755468e7fe582d23a0b7fd0691555
YahyaKhaliman <yahyaindrayana35@gmail.com>
2023-10-25 12:21:19 +0700

    Add stages deploy

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -5,14 +5,7 @@ echo 'Menjalankan aplikasi Python di port 5000...'
 # Instal dependensi Streamlit
 pip install streamlit
 
-# Jalankan aplikasi Streamlit
-streamlit run nama_app_streamlit.py
-
-
 # Menjalankan aplikasi Python di latar belakang
 python sources/app.py &
-
-# Menyimpan ID proses ke dalam file .pidfile
-echo $! > .pidfile
 
 echo 'Aplikasi sekarang berjalan. Kunjungi http://localhost:5000 untuk melihatnya.'
